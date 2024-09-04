220abbdaced241d5cb549b7f37bf7731fb45806b
Juan Espinosa <arq.jorrin@gmail.com>
2024-09-04 19:34:03 +0800

    Set up registration page and secure booking API

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
@@ -16,5 +17,6 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
   serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+  permission_classes = [IsAuthenticated]
   queryset = Booking.objects.all()
   serializer_class = BookingSerializer
